fc1634783bc5b5391f0e94106e8581f015410a57
Philipp Góes <phi_go@outlook.com>
2025-04-01 13:45:43 -0300

    Added Hangfire.SqlServer

--- src/EarthQuakeNews.Worker/Program.cs
+++ src/EarthQuakeNews.Worker/Program.cs
@@ -1,6 +1,9 @@
 using EarthQuakeNews.Infra.DI;
 using EarthQuakeNews.Infra.Job;
+using EarthQuakeNews.Infra.Settings;
 using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.Extensions.Options;
 
 namespace EarthQuakeNews.Worker
 {
@@ -11,10 +14,28 @@ namespace EarthQuakeNews.Worker
             var builder = Host.CreateApplicationBuilder(args);
 
             builder.Services.AddDependencies();
-            builder.Services.AddMemoryCache();
-            builder.Services.AddHangfire(config => config.UseInMemoryStorage());
-            builder.Services.AddHangfireServer();
 
+            //.Services.AddHangfire(config => config.UseInMemoryStorage());
+
+            builder.Services.AddHangfire(config =>
+            {
+                using var provider = builder.Services.BuildServiceProvider();
+                var rootSettings = provider.GetService<IOptions<RootSettings>>();
+
+                config.SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+                    .UseSimpleAssemblyNameTypeSerializer()
+                    .UseRecommendedSerializerSettings()
+                    .UseSqlServerStorage(rootSettings.Value.ConnectionString.SqlServer,
+                        new SqlServerStorageOptions
+                        {
+                            CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                            SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                            QueuePollInterval = TimeSpan.Zero,
+                            UseRecommendedIsolationLevel = true,
+                            DisableGlobalLocks = true,
+                        });
+            });
+            builder.Services.AddHangfireServer();
             builder.Services.AddHostedService<EarthquakeJob>();
             
             var host = builder.Build();
