f12cd2503733c493aa3d723411a99e90aedc98c1
Philipp Góes <phi_go@outlook.com>
2025-04-01 18:26:29 -0300

    Added unit tests from Controller and Applicaition

---
+++ test/EarthQuakeNews.UnitTest/Application/EarthquakeAppTests.cs
@@ -0,0 +1,280 @@
+﻿using EarthQuakeNews.Application.Earthquake;
+using EarthQuakeNews.Domain.DTOs;
+using EarthQuakeNews.Domain.Entities;
+using EarthQuakeNews.Domain.Interfaces.ExternalServices;
+using EarthQuakeNews.Domain.Interfaces.Repositories;
+using EarthQuakeNews.Domain.ValueObjects;
+using Moq;
+
+namespace EarthQuakeNews.UnitTest.Application
+{
+    public class EarthquakeAppTests
+    {
+        private readonly EarthquakeApp _earthquakeApp;
+        private readonly Mock<IEarthquakeUsgsExternalService> _earthquakeUsgsExternalServiceMock = new();
+        private readonly Mock<IEarthquakeRepository> _earthquakeRepositoryMock = new();
+        private readonly Mock<IEarthquakeCountRepository> _earthquakeCountRepositoryMock = new();
+
+        public EarthquakeAppTests()
+        {
+            _earthquakeApp = new EarthquakeApp(
+                _earthquakeUsgsExternalServiceMock.Object,
+                _earthquakeRepositoryMock.Object,
+                _earthquakeCountRepositoryMock.Object);
+        }
+
+        [Fact]
+        [Trait("App", "Earthquake")]
+        public async Task GetEarthquakeData__HasData_MustReturnEarthquakeData()
+        {
+            var data = GetEartquakesFromDb();
+
+            _earthquakeRepositoryMock
+                .Setup(e => e.GetEarthquakes())
+                .ReturnsAsync(data);
+
+            var result = await _earthquakeApp.GetEarthquakeData();
+
+            Assert.NotEmpty(result);
+        }
+
+        [Fact]
+        [Trait("App", "Earthquake")]
+        public async Task GetEarthquakeData__HasNoData_MustSaveEarthquakesData()
+        {
+            _earthquakeRepositoryMock
+                .SetupSequence(e => e.GetEarthquakes())
+                .ReturnsAsync(new List<Earthquake>())
+                .ReturnsAsync(new List<Earthquake>())
+                .ReturnsAsync(GetEartquakesFromDb());
+            _earthquakeUsgsExternalServiceMock
+                .Setup(e => e.GetEarthquakeCountToday())
+                .ReturnsAsync(3);
+            _earthquakeCountRepositoryMock
+                .Setup(e => e.GetCountToday())
+                .ReturnsAsync((EarthquakeCount?)null);
+            _earthquakeUsgsExternalServiceMock
+                .Setup(e => e.GetEarthquakeToday())
+                .ReturnsAsync(GetEartquakesFromExternalService());
+
+            var result = await _earthquakeApp.GetEarthquakeData();
+
+            Assert.NotEmpty(result);
+
+            _earthquakeCountRepositoryMock.Verify(e => e.Save(It.IsAny<EarthquakeCount>()), Times.Once);
+            _earthquakeRepositoryMock.Verify(e => e.SaveListAsync(It.IsAny<List<Earthquake>>()), Times.Once);
+        }
+
+        [Fact]
+        [Trait("App", "Earthquake")]
+        public async Task Execute__HasNoData_MustSaveEarthquakesData()
+        {
+            _earthquakeRepositoryMock
+                .SetupSequence(e => e.GetEarthquakes())
+                .ReturnsAsync(new List<Earthquake>())
+                .ReturnsAsync(GetEartquakesFromDb());
+            _earthquakeUsgsExternalServiceMock
+                .Setup(e => e.GetEarthquakeCountToday())
+                .ReturnsAsync(3);
+            _earthquakeCountRepositoryMock
+                .Setup(e => e.GetCountToday())
+                .ReturnsAsync((EarthquakeCount?)null);
+            _earthquakeUsgsExternalServiceMock
+                .Setup(e => e.GetEarthquakeToday())
+                .ReturnsAsync(GetEartquakesFromExternalService());
+
+            var result = await _earthquakeApp.Execute();
+
+            Assert.NotEmpty(result);
+
+            _earthquakeCountRepositoryMock.Verify(e => e.Save(It.IsAny<EarthquakeCount>()), Times.Once);
+            _earthquakeRepositoryMock.Verify(e => e.SaveListAsync(It.IsAny<List<Earthquake>>()), Times.Once);
+        }
+
+        [Fact]
+        [Trait("App", "Earthquake")]
+        public async Task Execute__HasDifferentCountData_MustReturnUpdatedEarthquakeData()
+        {
+            _earthquakeRepositoryMock
+                .SetupSequence(e => e.GetEarthquakes())
+                .ReturnsAsync(GetEartquakesFromDb())
+                .ReturnsAsync(GetEartquakesFromDbNewData());
+            _earthquakeUsgsExternalServiceMock
+                .Setup(e => e.GetEarthquakeCountToday())
+                .ReturnsAsync(4);
+            _earthquakeCountRepositoryMock
+                .Setup(e => e.GetCountToday())
+                .ReturnsAsync(new EarthquakeCount(3));
+            _earthquakeUsgsExternalServiceMock
+                .Setup(e => e.GetEarthquakeToday())
+                .ReturnsAsync(GetEartquakesFromExternalServiceNewData());
+
+            var result = await _earthquakeApp.Execute();
+
+            Assert.NotEmpty(result);
+            Assert.Equal(4, result.Count());
+
+            _earthquakeCountRepositoryMock.Verify(e => e.Update(It.IsAny<int>()), Times.Once);
+            _earthquakeRepositoryMock.Verify(e => e.SaveListAsync(It.IsAny<List<Earthquake>>()), Times.Once);
+        }
+
+        [Fact]
+        [Trait("App", "Earthquake")]
+        public async Task Execute__HasSameCountData_MustReturnEarthquakeData()
+        {
+            _earthquakeRepositoryMock
+                .SetupSequence(e => e.GetEarthquakes())
+                .ReturnsAsync(GetEartquakesFromDb());
+            _earthquakeUsgsExternalServiceMock
+                .Setup(e => e.GetEarthquakeCountToday())
+                .ReturnsAsync(3);
+            _earthquakeCountRepositoryMock
+                .Setup(e => e.GetCountToday())
+                .ReturnsAsync(new EarthquakeCount(3));
+
+            var result = await _earthquakeApp.Execute();
+
+            Assert.NotEmpty(result);
+        }
+
+        private static IEnumerable<Earthquake> GetEartquakesFromDb()
+        {
+            return new List<Earthquake>
+            {
+                new(0.76, "10 km WNW of The Geysers, CA", new Latitude(38.806835), new Longitude(-122.865669), 1.3500, DateTime.UtcNow, "nc75158552", "https://earthquake.usgs.gov/earthquakes/eventpage/nc75158552"),
+                new(0.90, "35 km NNW of Beluga, Alaska", new Latitude(61.440900), new Longitude(-151.331000), 64.8000, DateTime.UtcNow, "ak02546rfm3u", "https://earthquake.usgs.gov/earthquakes/eventpage/ak02546rfm3u"),
+                new(0.76, "66 km NNE of Petersville, Alaska", new Latitude(63.061500), new Longitude(-150.342900), 105.5000, DateTime.UtcNow, "ak02546r3kto", "https://earthquake.usgs.gov/earthquakes/eventpage/ak02546r3kto")
+            };
+        }
+
+        private static EarthquakeInfoDto[] GetEartquakesFromExternalService()
+        {
+            return
+            [
+                new EarthquakeInfoDto
+                {
+                    Properties = new Properties
+                    {
+                        Mag = 0.76,
+                        Place = "10 km WNW of The Geysers, CA",
+                        Time = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+                        Code = "nc75158552",
+                        Url = "https://earthquake.usgs.gov/earthquakes/eventpage/nc75158552"
+                    },
+                    Geometry = new Geometry
+                    {
+                        Coordinates = [-122.865669, 38.806835, 1.3500]
+                    }
+                },
+                new EarthquakeInfoDto
+                {
+                    Properties = new Properties
+                    {
+                        Mag = 0.90,
+                        Place = "35 km NNW of Beluga, Alaska",
+                        Time = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+                        Code = "ak02546rfm3u",
+                        Url = "https://earthquake.usgs.gov/earthquakes/eventpage/ak02546rfm3u"
+                    },
+                    Geometry = new Geometry
+                    {
+                        Coordinates = [-151.331000, 61.440900, 64.8000]
+                    }
+                },
+                new EarthquakeInfoDto
+                {
+                    Properties = new Properties
+                    {
+                        Mag = 0.76,
+                        Place = "66 km NNE of Petersville, Alaska",
+                        Time = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+                        Code = "ak02546r3kto",
+                        Url = "https://earthquake.usgs.gov/earthquakes/eventpage/ak02546r3kto"
+                    },
+                    Geometry = new Geometry
+                    {
+                        Coordinates = [-150.342900, 63.061500, 105.5000]
+                    }
+                }
+            ];
+        }
+
+        private static IEnumerable<Earthquake> GetEartquakesFromDbNewData()
+        {
+            return new List<Earthquake>
+            {
+                new(0.76, "10 km WNW of The Geysers, CA", new Latitude(38.806835), new Longitude(-122.865669), 1.3500, DateTime.UtcNow, "nc75158552", "https://earthquake.usgs.gov/earthquakes/eventpage/nc75158552"),
+                new(0.90, "35 km NNW of Beluga, Alaska", new Latitude(61.440900), new Longitude(-151.331000), 64.8000, DateTime.UtcNow, "ak02546rfm3u", "https://earthquake.usgs.gov/earthquakes/eventpage/ak02546rfm3u"),
+                new(0.76, "66 km NNE of Petersville, Alaska", new Latitude(63.061500), new Longitude(-150.342900), 105.5000, DateTime.UtcNow, "ak02546r3kto", "https://earthquake.usgs.gov/earthquakes/eventpage/ak02546r3kto"),
+                new(1.80, "30 km NW of Toyah, Texas", new Latitude(31.520000), new Longitude(-104.011000), 7.5867, DateTime.UtcNow, "2025gjqh", "https://earthquake.usgs.gov/earthquakes/eventpage/2025gjqh"),
+            };
+        }
+
+        private static EarthquakeInfoDto[] GetEartquakesFromExternalServiceNewData()
+        {
+            return
+            [
+                new EarthquakeInfoDto
+                {
+                    Properties = new Properties
+                    {
+                        Mag = 0.76,
+                        Place = "10 km WNW of The Geysers, CA",
+                        Time = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+                        Code = "nc75158552",
+                        Url = "https://earthquake.usgs.gov/earthquakes/eventpage/nc75158552"
+                    },
+                    Geometry = new Geometry
+                    {
+                        Coordinates = [-122.865669, 38.806835, 1.3500]
+                    }
+                },
+                new EarthquakeInfoDto
+                {
+                    Properties = new Properties
+                    {
+                        Mag = 0.90,
+                        Place = "35 km NNW of Beluga, Alaska",
+                        Time = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+                        Code = "ak02546rfm3u",
+                        Url = "https://earthquake.usgs.gov/earthquakes/eventpage/ak02546rfm3u"
+                    },
+                    Geometry = new Geometry
+                    {
+                        Coordinates = [-151.331000, 61.440900, 64.8000]
+                    }
+                },
+                new EarthquakeInfoDto
+                {
+                    Properties = new Properties
+                    {
+                        Mag = 0.76,
+                        Place = "66 km NNE of Petersville, Alaska",
+                        Time = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+                        Code = "ak02546r3kto",
+                        Url = "https://earthquake.usgs.gov/earthquakes/eventpage/ak02546r3kto"
+                    },
+                    Geometry = new Geometry
+                    {
+                        Coordinates = [-150.342900, 63.061500, 105.5000]
+                    }
+                },
+                new EarthquakeInfoDto
+                {
+                    Properties = new Properties
+                    {
+                        Mag = 1.80,
+                        Place = "30 km NW of Toyah, Texas",
+                        Time = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+                        Code = "2025gjqh",
+                        Url = "https://earthquake.usgs.gov/earthquakes/eventpage/2025gjqh"
+                    },
+                    Geometry = new Geometry
+                    {
+                        Coordinates = [-104.011000, 31.520000, 7.5867]
+                    }
+                }
+            ];
+        }
+    }
+}
---
+++ test/EarthQuakeNews.UnitTest/Controller/EarthquakeControllerTests.cs
@@ -0,0 +1,84 @@
+﻿using EarthQuakeNews.Domain.Interfaces.Application;
+using EarthQuakeNews.Domain.ViewModel;
+using EarthQuakeNews.WebApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace EarthQuakeNews.UnitTest.Controller
+{
+    public class EarthquakeControllerTests
+    {
+        private readonly EarthquakeController _earthquakeController;
+        private readonly Mock<IEarthquakeApp> _earthquakeAppMock = new();
+
+        public EarthquakeControllerTests()
+        {
+            _earthquakeController = new EarthquakeController();
+        }
+
+        [Fact]
+        [Trait("Controller", "Earthquake")]
+        public async Task Execute__HasData_MustReturnOk()
+        {
+            var response = GetEartquakes();
+
+            _earthquakeAppMock
+                .Setup(x => x.GetEarthquakeData())
+                .ReturnsAsync(response);
+
+            var result = await _earthquakeController.GetEarthquakeInfo(_earthquakeAppMock.Object);
+
+            Assert.IsAssignableFrom<OkObjectResult>(result);
+        }
+
+        [Fact]
+        [Trait("Controller", "Earthquake")]
+        public async Task Execute__HasNoData_MustReturnNoContent()
+        {
+            _earthquakeAppMock
+                .Setup(x => x.GetEarthquakeData())
+                .ReturnsAsync(new List<EarthquakeInfoViewModel>());
+
+            var result = await _earthquakeController.GetEarthquakeInfo(_earthquakeAppMock.Object);
+
+            Assert.IsAssignableFrom<NoContentResult>(result);
+        }
+
+        private static IEnumerable<EarthquakeInfoViewModel> GetEartquakes()
+        {
+            return new List<EarthquakeInfoViewModel>
+            {
+                new()
+                {
+                    Magnitude = 0.76,
+                    Place = "10 km WNW of The Geysers, CA",
+                    Latitude = 38.806835,
+                    Longitude = -122.865669,
+                    KmDepth = 1.3500,
+                    EarthquakeTime = DateTime.UtcNow,
+                    Url = "https://earthquake.usgs.gov/earthquakes/eventpage/nc75158552"
+                },
+                new()
+                {
+                    Magnitude = 0.90,
+                    Place = "35 km NNW of Beluga, Alaska",
+                    Latitude = 61.440900,
+                    Longitude = -151.331000,
+                    KmDepth = 64.8000,
+                    EarthquakeTime = DateTime.UtcNow,
+                    Url = "https://earthquake.usgs.gov/earthquakes/eventpage/ak02546rfm3u"
+                },
+                new()
+                {
+                    Magnitude = 0.76,
+                    Place = "66 km NNE of Petersville, Alaska",
+                    Latitude = 63.061500,
+                    Longitude = -150.342900,
+                    KmDepth = 105.5000,
+                    EarthquakeTime = DateTime.UtcNow,
+                    Url = "https://earthquake.usgs.gov/earthquakes/eventpage/ak02546r3kto"
+                }
+            };
+        }
+    }
+}
