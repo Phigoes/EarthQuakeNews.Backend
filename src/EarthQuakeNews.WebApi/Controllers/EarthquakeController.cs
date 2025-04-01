4da11705abc49df5ee1b914c152067afa3f95437
Philipp Góes <phi_go@outlook.com>
2025-04-01 16:35:55 -0300

    Added NoContent

--- src/EarthQuakeNews.WebApi/Controllers/EarthquakeController.cs
+++ src/EarthQuakeNews.WebApi/Controllers/EarthquakeController.cs
@@ -10,10 +10,14 @@ namespace EarthQuakeNews.WebApi.Controllers
     {
         [HttpGet]
         [ProducesResponseType<IEnumerable<EarthquakeInfoViewModel>>(StatusCodes.Status200OK)]
+        [ProducesResponseType<IEnumerable<EarthquakeInfoViewModel>>(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetEarthquakeInfo([FromServices] IEarthquakeApp app)
         {
             var response = await app.GetEarthquakeData();
+
+            if (!response.Any())
+                return NoContent();
 
             return Ok(response);
         }
